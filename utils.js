d4db9a78efb452dde1cf6b89bea53b7a8a109d6c
alexbarksdale <ale.barksdale@gmail.com>
2020-06-29 12:29:39 -0500

    Util file complete and basic tests

---
+++ utils.js
@@ -0,0 +1,58 @@
+// ========================================================
+// Level 1 Challenges
+// ========================================================
+
+const sayHello = () => {
+    return 'Hello';
+};
+
+// should return the area
+const area = (w, h) => w * h;
+
+// should return the perimeter
+const perimeter = (w, h) => 2 * w + 2 * h;
+
+// should return the area of the circle
+const circleArea = (r) => Math.pow(r, 2) * Math.PI;
+
+// ========================================================
+// Level 2 Challenges
+// ========================================================
+// NOTE: You will need to implement methods below (not yet
+// defined) in order to make the tests pass.
+// ========================================================
+
+const shoppingCart = [];
+
+const clearCart = () => {
+    shoppingCart.length = 0;
+};
+
+const createItem = (name, price) => {
+    return { name, price, quantity: 1 };
+};
+
+// should return the current state of shopping cart
+const getShoppingCart = () => shoppingCart;
+
+// should add item to shopping cart
+const addItemToCart = (item) => shoppingCart.append(item);
+
+// should return the total quantity of items in cart
+const getNumItemsInCart = () => shoppingCart.length;
+
+// should remove item from shopping cart
+const removeItemFromCart = (item) => shoppingCart.filter((cartItem) => cartItem != item);
+
+module.exports = {
+    sayHello,
+    area,
+    perimeter,
+    circleArea,
+    clearCart,
+    createItem,
+    getShoppingCart,
+    addItemToCart,
+    getNumItemsInCart,
+    removeItemFromCart,
+};
